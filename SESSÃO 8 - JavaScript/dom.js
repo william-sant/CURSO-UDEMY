03c214692f93e74bcf6d275409c2ad6dd7215ed5
William Santana <81190663+william-sant@users.noreply.github.com>
2023-02-02 15:32:20 -0300

    eventos dom e app calculadora

---
+++ SESSÃO 8 - JavaScript/dom.js
@@ -0,0 +1,9 @@
+function modificaEstilo (corDeFundo, l, a) {
+    document.getElementById('quadrado').style.background = corDeFundo
+    document.getElementById('quadrado').style.width = l
+    document.getElementById('quadrado').style.width = a
+}
+
+function modificaClasse() {
+    document.getElementById('mensagem').className = 'estilo2'
+}
